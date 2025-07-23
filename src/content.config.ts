a768eefefa107eb4a7365a5e6d7f8022a9e01fb0
Adrian Tang <adrian.s.tang@gmail.com>
2025-07-23 05:23:17 -0400

    Set up Projects

--- src/content.config.ts
+++ src/content.config.ts
@@ -15,4 +15,19 @@ const blog = defineCollection({
 	}),
 });
 
-export const collections = { blog };
+const projects = defineCollection({
+	// Type-check frontmatter using a schema
+	schema: ({ image }) => z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: image().optional(),
+	}),
+});
+
+export const collections = { 
+	blog,
+	projects
+};
---
+++ src/content/projects/first-project.md
@@ -0,0 +1,8 @@
+---
+title: 'First Project'
+description: 'This is my first project.'
+pubDate: 'Jul 23 2025'
+heroImage: '../../assets/blog-placeholder-1.jpg'
+---
+
+This is the content of my first project.
