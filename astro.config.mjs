f243bf3d1b38f72f0a43fcfa3de1932c1cb07c04
Adrian Tang <adrian.s.tang@gmail.com>
2025-07-24 15:41:56 -0400

    Add path aliasing for src

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,20 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+  },
 });
